c00d80ce7441b49186572fcbafb983847c2971d2
SBCLockDevTeam <LockDevTeam@securitybuildingcontrols.com>
2026-03-25 07:37:53 -0400

    Create menu.php file with navigation links

---
+++ templates/common/menu.php
@@ -0,0 +1,19 @@
+<?php
+
+// PHP menu template with navigation links
+
+echo '<nav>';
+
+echo '<ul>';
+
+echo '<li><a href="home.php">Home</a></li>';
+
+echo '<li><a href="inventory.php">Inventory</a></li>';
+
+echo '<li><a href="statistics.php">Statistics</a></li>';
+
+echo '<li><a href="admin.php">Admin</a></li>';
+
+echo '</ul>';
+
+echo '</nav>';
