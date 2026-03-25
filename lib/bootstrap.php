2f691a421d504cc48f4f212014c201df00295cfa
SBCLockDevTeam <LockDevTeam@securitybuildingcontrols.com>
2026-03-25 10:00:42 -0400

    Create bootstrap.php with helper functions

---
+++ lib/bootstrap.php
@@ -0,0 +1,17 @@
+<?php
+// Function to connect to the database
+function db() {
+    // Database connection code goes here
+}
+
+function add_error($error) {
+    // Code to add error goes here
+}
+
+function get_errors() {
+    // Code to retrieve errors goes here
+}
+
+function log_exception($exception) {
+    // Code to log exceptions goes here
+}
