c1d1d19913904a9fbcd08034aa5e33b54ffb22e0
SBCLockDevTeam <LockDevTeam@securitybuildingcontrols.com>
2026-03-26 18:02:49 -0400

    Add common menu template for UI

--- templates/common/menu.php
+++ templates/common/menu.php
@@ -1,28 +1,10 @@
 <?php
-/**
- * Menu Template
- * 
- * Primary horizontal navigation menu with dropdowns
- * Menu items data-driven based on user role
- */
+// Common menu template
 ?>
 <nav class="main-menu">
-    <ul class="menu-items">
-        <li class="menu-item">
-            <a href="/qr/admin/">Dashboard</a>
-        </li>
-        <li class="menu-item">
-            <a href="/qr/admin/items/">Items</a>
-            <ul class="submenu">
-                <li><a href="/qr/admin/items/">List Items</a></li>
-                <li><a href="/qr/admin/items/add.php">Add Item</a></li>
-            </ul>
-        </li>
-        <li class="menu-item">
-            <a href="/qr/admin/brands/">Brands</a>
-        </li>
-        <li class="menu-item">
-            <a href="/qr/admin/logs/">Logs</a>
-        </li>
+    <ul>
+        <li><a href="/index.php">Home</a></li>
+        <li><a href="/admin/items/index.php">Items</a></li>
+        <!-- Add more menu items as needed -->
     </ul>
 </nav>
