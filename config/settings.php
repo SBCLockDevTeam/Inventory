aa338b78b625775446adc951fc31f026b2e2beb4
SBCLockDevTeam <LockDevTeam@securitybuildingcontrols.com>
2026-03-25 20:06:46 -0400

    Delete config directory

--- config/settings.php
+++
@@ -1,7 +0,0 @@
-<?php
-return [
-    'app_version' => '1.0.0',
-    'root_url' => '/qr/',
-    'app_name' => 'SBC Inventory',
-    'app_description' => 'Security Building Controls Inventory Management System',
-];
