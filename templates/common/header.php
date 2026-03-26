25e064b2d51980b54dfdd4a67bbbcb49ea3f475e
SBCLockDevTeam <LockDevTeam@securitybuildingcontrols.com>
2026-03-26 19:32:30 -0400

    Update header.php with new contents.

--- templates/common/header.php
+++ templates/common/header.php
@@ -3,16 +3,6 @@
 require_once __DIR__ . '/../../config/settings.php';
 $page_title = $page_title ?? 'QR Inventory System';
 ?>
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title><?php echo htmlspecialchars($page_title); ?></title>
-    <link rel="stylesheet" href="<?php echo CSS_PATH; ?>style.css">
-    <script src="<?php echo JS_PATH; ?>script.js" defer></script>
-</head>
-<body>
 <header>
     <div class="header-content">
         <div class="logo">
