7c429603e25b65d5d6e3cc9fcdc286242343dfcc
SBCLockDevTeam <LockDevTeam@securitybuildingcontrols.com>
2026-03-26 19:25:37 -0400

    Updated lib/database.php with the new DatabaseHelper implementation.

--- lib/database.php
+++ lib/database.php
@@ -23,7 +23,7 @@ class DatabaseHelper {
 
         try {
             $dsn = 'mysql:host=' . DB_HOST . ';dbname=' . DB_NAME . ';charset=utf8mb4';
-            self::$connection = new PDO($dsn, DB_USER, DB_PASSWORD, [
+            self::$connection = new PDO($dsn, DB_USER, DB_PASS, [
                 PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
                 PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
             ]);
