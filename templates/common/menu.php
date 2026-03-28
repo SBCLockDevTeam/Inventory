d3415c90ecbe7216f2e3a8d8c09d9af823e35074
SBCLockDevTeam <LockDevTeam@securitybuildingcontrols.com>
2026-03-28 15:05:17 -0400

    Update menu.php to reflect the current menu structure as of 2026-03-28 19:04:52 UTC

--- templates/common/menu.php
+++ templates/common/menu.php
@@ -1,4 +1,4 @@
-<?php
+'<?php
 require_once __DIR__ . '/../../config/settings.php';
 require_once __DIR__ . '/../../lib/client_helper.php';
 $_menu_is_admin = ClientHelper::isActiveUserAdmin();
@@ -6,7 +6,7 @@ $_menu_is_admin = ClientHelper::isActiveUserAdmin();
 <nav class="menu">
     <button class="hamburger-btn" id="menu-hamburger" aria-expanded="false" aria-controls="main-menu-list">☰</button>
     <ul class="menu-list" id="main-menu-list">
-        <li><a href="<?php echo BASE_PATH; ?>/home.php">Home</a></li>
+        <li><a href="<?php echo BASE_PATH; ?>/items/">Home</a></li>
         <li><a href="<?php echo BASE_PATH; ?>/items/">Items</a>
             <ul class="submenu">
                 <li><a href="<?php echo BASE_PATH; ?>/admin/items/add.php">Add Item</a></li>
@@ -26,4 +26,4 @@ $_menu_is_admin = ClientHelper::isActiveUserAdmin();
         </li>
         <?php endif; ?>
     </ul>
-</nav>
+</nav>'
