8931ad806f98169846a1e40fb609493e5847b536
SBCLockDevTeam <LockDevTeam@securitybuildingcontrols.com>
2026-03-29 01:13:02 -0400

    Update print.php for horizontal and vertical positioning

--- api/print.php
+++ api/print.php
@@ -79,10 +79,10 @@ $payload .= "\x1Bk\x0B\x1BX\x00\x30\x00";
 $payload .= $item_name;
 
 
-$payload .= "\x1B$\x40\x00";							// Absolute Horozontal Position
-$payload .= "\x1B(V\x02\x00\x20\x00";					// Absolute Vertical Position
-//$payload .= "\x1Bk\x0B\x1BX\x00&\x00";
-$payload .= "\x1Bk\x0B\x1BX\x00\x30\x00";
+$payload .= "\x1B$\xA0\x00";							// Absolute Horozontal Position
+$payload .= "\x1B(V\x02\x00\x60\x00";					// Absolute Vertical Position
+$payload .= "\x1Bk\x0B\x1BX\x00&\x00";
+//$payload .= "\x1Bk\x0B\x1BX\x00\x30\x00";
 
 	
 if (trim($description) !== '') {
