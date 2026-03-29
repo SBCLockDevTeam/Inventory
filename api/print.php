6d44693f68ef1ae1a9f5263eeae52e82df7b7d4e
SBCLockDevTeam <LockDevTeam@securitybuildingcontrols.com>
2026-03-28 23:44:50 -0400

    Update print payload for font selection

--- api/print.php
+++ api/print.php
@@ -65,9 +65,10 @@ $payload .= "\x1b\x6b\x0b";                             // Select font (Letter G
 //$payload .= "\x1b\x45";								  // Turn on bold
 //$payload .= "\x1B\x46";							      // Turn off bold
 
+$payload .= "\x1B\x30\x00";
 	
 
-$payload .= "\x1B$\x30\x00";
+$payload .= "\x1B$\x18\x00";
 $payload .= "\x1B(V\x02\x00\x0C\x00";
 $payload .= "\x1Bk\x0B\x1BX\x00&\x00";
 
