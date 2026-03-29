def8c07b6c997bd7049c1565251a2ae8f11dfcd8
SBCLockDevTeam <LockDevTeam@securitybuildingcontrols.com>
2026-03-29 00:49:41 -0400

    Fix payload for QR code generation

--- api/print.php
+++ api/print.php
@@ -82,7 +82,7 @@ if (trim($description) !== '') {
 
 $payload .= "\x1B$\x08\x00";							// Absolute Horozontal Position
 $payload .= "\x1B(V\x22\x00\x0C\x00";					// Absolute Vertical Position
-$payload .="\x1b\x69\x51\x08\x02\x00\x00\x00\x00\x04\x00";
+$payload .="\x1b\x69\x51\x07\x02\x00\x00\x00\x00\x04\x00";
 $payload .="https://sbcqr.com/qr/?Q=792ddc3f5c";
 $payload .="\x5c\x5c\x5c";
 
